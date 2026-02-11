61ee1cfd4ed9933e397ca2be68ca0c72f6080625
zll <1570040520@qq.com>
2026-02-11 17:47:16 +0800

    更正配置文件

---
+++ harbor-gateway/src/main/java/com/harbor/HarborGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.harbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarborGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarborGatewayApplication.class, args);
+    }
+
+}
---
+++ harbor-second-hand/src/main/java/com/harbor/HarborSecondHandApplication.java
@@ -0,0 +1,13 @@
+package com.harbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarborSecondHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarborSecondHandApplication.class, args);
+    }
+
+}
